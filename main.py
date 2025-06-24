319af593b53e869f37c47b0d019a113e2e5754cc
AleksaAleksZakharov <AleksaAleksZakharov@Greenatom.ru>
2025-06-24 18:03:17 +0300

    3

--- main.py
+++
@@ -1,7 +0,0 @@
-from methods.internvl_utils import retrieve_logit_lens_internvl, load_internvl_state
-from methods.algorithms import internal_confidence, internal_confidence_heatmap, internal_confidence_segmentation
-from PIL import Image
-from matplotlib import pyplot as plt
-import numpy as np
-
-model_state = load_internvl_state()
